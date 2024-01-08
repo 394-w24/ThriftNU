df6e78bf80a656c7e6cccfae9923a06d80a1510b
victorchen45678 <93490644+victorchen45678@users.noreply.github.com>
2024-01-08 10:33:17 -0600

    added modal

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,14 +1,14 @@
-const Item = ({item}) => {
-    const { condition, name, price, imageURL, seller } = item;
-    return (
-        <div>
-            <h3>{name}</h3>
-            <p>Price: ${price}</p>
-            <p>Condition: {condition}</p>
-            <p>Sold by: {seller}</p>
-            <img src={imageURL} alt="product" />
-        </div>
-    );
-}
+const Item = ({ item }) => {
+  const { condition, name, price, imageURL, seller } = item;
+  return (
+    <div>
+      <h3>{name}</h3>
+      <p>Price: ${price}</p>
+      <p>Condition: {condition}</p>
+      <p>Sold by: {seller}</p>
+      <img src={imageURL} alt="product" />
+    </div>
+  );
+};
 
 export default Item;
