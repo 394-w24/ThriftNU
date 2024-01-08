71ee1fde3e8477e63aefe5ea4ec88c65bbc5b69f
unknown <2263283337@qq.com>
2024-01-08 12:47:34 -0600

    corrected image and slightly improved UI

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,17 +1,19 @@
-import './Item.css';
+import "./Item.css";
 
-const Item = ({item}) => {
-    const { condition, name, price, imageURL, seller } = item;
-    return (
-        <div className='Item'>
-            <div className='Info'>
-                <h3>{name}</h3>
-                <p>Price: ${price}</p>
-                <p>Condition: {condition}</p>
-                <p>Sold by: {seller}</p>
-            </div>
-            <img className='Img' src={imageURL} alt="product" />
-        </div>
-    );
-}
+const Item = ({ item }) => {
+  const { condition, name, price, imageURL, seller, email, description } = item;
+  return (
+    <div className="Item">
+      <div className="Info">
+        <h3>{name}</h3>
+        <p>Price: ${price}</p>
+        <p>Condition: {condition}</p>
+        <p>Sold by: {seller}</p>
+        <p>Seller Email: {email}</p>
+        <p>Description: {description}</p>
+      </div>
+      <img className="Img" src={imageURL} alt="product" />
+    </div>
+  );
+};
 export default Item;
