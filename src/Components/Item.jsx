45e7cac9e8ecb97ef5ccae4352abf70a84a61245
Abel Abong <abelabong@Abels-MacBook-Pro-2.local>
2024-01-08 14:14:11 -0600

    CSS Styling

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,12 +1,16 @@
-const Item = ({item}) => {
+import './Item.css';
+
+const Item = ({ item }) => {
     const { condition, name, price, imageURL, seller } = item;
     return (
-        <div>
-            <h3>{name}</h3>
-            <p>Price: ${price}</p>
-            <p>Condition: {condition}</p>
-            <p>Sold by: {seller}</p>
-            <img src={imageURL} alt="product" />
+        <div className="Item">
+            <img className="Image" src={imageURL} alt="product" />
+            <div className="Info">
+                <h3>{name}</h3>
+                <p>Price: ${price}</p>
+                <p>Condition: {condition}</p>
+                <p>Sold by: {seller}</p>
+            </div>
         </div>
     );
 }
