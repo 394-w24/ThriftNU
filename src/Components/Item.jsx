37af37907ac219114891d93fbc98abce71e0d05f
achen11 <53618106+achen11@users.noreply.github.com>
2024-01-08 10:55:18 -0600

    Item css changes

--- src/Components/Item.jsx
+++ src/Components/Item.jsx
@@ -1,12 +1,16 @@
+import './Item.css';
+
 const Item = ({item}) => {
     const { condition, name, price, imageURL, seller } = item;
     return (
-        <div>
-            <h3>{name}</h3>
-            <p>Price: ${price}</p>
-            <p>Condition: {condition}</p>
-            <p>Sold by: {seller}</p>
-            <img src={imageURL} alt="product" />
+        <div className='Item'>
+            <div className='Info'>
+                <h3>{name}</h3>
+                <p>Price: ${price}</p>
+                <p>Condition: {condition}</p>
+                <p>Sold by: {seller}</p>
+            </div>
+            <img className='Img' src={imageURL} alt="product" />
         </div>
     );
 }
