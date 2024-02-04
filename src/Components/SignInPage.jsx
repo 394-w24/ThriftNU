4aee3bcdb36b8f02ec1cbcf3b93bcf45fa15a9da
ArikoAbel <abongabel28@gmail.com>
2024-02-04 09:52:24 -0600

    Update SignInPage.jsx
    log in function working

--- src/Components/SignInPage.jsx
+++ src/Components/SignInPage.jsx
@@ -1,33 +1,22 @@
 import React, { useEffect, useState } from 'react';
 import thriftNuLogo from './logo.png';
 import { useNavigate } from 'react-router-dom';
-import "./SignInPage.css"; 
+import "./SignInPage.css";
+import {useAuthState} from "react-firebase-hooks/auth";
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import {auth} from "../firebase";
 function SignInPage() {
-  const [registerEmail, setRegisterEmail] = useState('');
-  const [registerPassword, setRegisterPassword] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [user, loading, error]= useAuthState(auth);
   
   const navigate = useNavigate();
-  const [error, setError] = useState('');
-  const handleRegister = async (event) => {
-    event.preventDefault();
-    try {
-      const userCredential = await createUserWithEmailAndPassword(auth, registerEmail, registerPassword);
-      console.log('Registered user:', userCredential.user);
-      // You can automatically sign the user in, or show a success message, etc.
-    } catch (err) {
-      console.error('Registration error:', err);
-      setError(err.message); // Set error message to display to the user
-    }
-  };
   const handleSubmit = async (event) => {
     event.preventDefault();
-
-    // Simulate a successful sign-in
-    navigate('/home');
+    signInWithEmailAndPassword(email, password);
+    navigate('/home')
   };
+  
 
   return (
     <div className="signin-container">
@@ -62,30 +51,6 @@ function SignInPage() {
             />
           </div>
           <button type="submit">Log In</button>
-          onClick={() => signInWithEmailAndPassword(email, password)}
-        </form>
-        <form onSubmit={handleRegister}>
-          <div className="form-group">
-            <label>Register Email:</label>
-            <input 
-              type="email" 
-              placeholder="Enter email for registration"
-              value={registerEmail} 
-              onChange={(e) => setRegisterEmail(e.target.value)} 
-              required 
-            />
-          </div>
-          <div className="form-group">
-            <label>Register Password:</label>
-            <input 
-              type="password" 
-              placeholder="Enter password for registration"
-              value={registerPassword} 
-              onChange={(e) => setRegisterPassword(e.target.value)} 
-              required 
-            />
-          </div>
-          <button type="submit">Register</button>
         </form>
       </div>
     </div>
