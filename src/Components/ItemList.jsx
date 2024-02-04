832b5861099595a9c463527ce7c4e36e7147eccf
Mercy Omwoyo <mercyomwoyo@Mercys-MacBook-Air.local>
2024-02-04 11:37:58 -0600

    Fixed some css for itemList.jsx that was bringing some warnings about formatting

--- src/Components/ItemList.jsx
+++ src/Components/ItemList.jsx
@@ -9,11 +9,13 @@ const ItemList = ({items, setSelectedItem, openModal, dropValue}) => {
     
 
     return (
-        <div style={{'display': 'flex', 'justify-content': 'space-evenly', 'flex-wrap': 'wrap', 'align-items': 'bottom'}}>
+        <div style={{ display: 'flex', justifyContent: 'space-evenly', flexWrap: 'wrap', alignItems: 'flex-end' }}>
             {Object.values(items).filter((product) => dropValue ? product.subject === dropValue : true).map((item, id) =>
-                <Item key={id} item={item} click={() => clickHandler(item)} />)}
+                <Item key={id} item={item} click={() => clickHandler(item)} />
+            )}
         </div>
     );
+    
 }
 
 export default ItemList;
